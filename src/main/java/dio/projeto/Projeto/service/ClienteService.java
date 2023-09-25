f96203965acd0f7dbe6ebc475f6aa932d5e9576b
CaiqueDiasp <caiquediasp@gmail.com>
2023-09-25 12:01:09 -0300

    Desafio Padroes de Projeto - RestApi + viacep API

---
+++ src/main/java/dio/projeto/Projeto/service/ClienteService.java
@@ -0,0 +1,15 @@
+package dio.projeto.Projeto.service;
+
+import dio.projeto.Projeto.model.Cliente;
+
+public interface ClienteService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
---
+++ src/main/java/dio/projeto/Projeto/service/ViaCepService.java
@@ -0,0 +1,17 @@
+package dio.projeto.Projeto.service;
+
+import dio.projeto.Projeto.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+@Repository
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
